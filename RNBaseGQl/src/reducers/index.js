4b5c412db58adbb2f0c24402104cee298adbe579
SimaRajput <simarrajput12@gmail.com>
2021-05-10 17:35:34 +0530

    update react navigation

--- RNBaseGQl/src/reducers/index.js
+++ RNBaseGQl/src/reducers/index.js
@@ -2,20 +2,19 @@ import AsyncStorage from '@react-native-community/async-storage';
 import { persistCombineReducers } from 'redux-persist';
 import { toastReducer as toast } from 'react-native-redux-toast';
 import app from './app';
-import nav from './nav';
+// import nav from './nav';
 import user from './user';
 import language from './language';
 import home from './home';
 
 const config = {
-  blacklist: ['app', 'nav', 'toast'],
+  blacklist: ['app', 'toast'],
   key: 'primary',
   storage: AsyncStorage,
 };
 
 const reducers = persistCombineReducers(config, {
   app,
-  nav,
   toast,
   user,
   language,
