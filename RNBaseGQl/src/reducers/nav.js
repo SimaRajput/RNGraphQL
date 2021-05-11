9f333544c59ac1814a791c290c4044497c495698
Ritika Garg <34366176+rgarg22@users.noreply.github.com>
2021-05-11 10:31:33 +0530

    Revert "Masterclone"

--- RNBaseGQl/src/reducers/nav.js
+++ RNBaseGQl/src/reducers/nav.js
@@ -1,5 +1,5 @@
 import Idx from 'idx';
-import { CommonActions } from '@react-navigation/native';
+import { NavigationActions, StackActions } from 'react-navigation';
 import { REHYDRATE } from 'redux-persist';
 import { AppNavigator } from '../config/navigator';
 import { GO_BACK, RESET_NAVIGATOR } from '../actions/nav-action-types';
@@ -7,7 +7,7 @@ import { LOGOUT_SUCCESS,LOGIN_SUCCESS } from '../actions/user-actions-types';
 
 const initialRoute = 'Loader';
 const initialState = AppNavigator.router.getStateForAction(
-  AppNavigator?.router?.getActionForPathAndParams(initialRoute)
+  AppNavigator.router.getActionForPathAndParams(initialRoute)
 );
 
 export default function nav(state = initialState, action) {
@@ -24,9 +24,9 @@ export default function nav(state = initialState, action) {
   switch (type) {
     case RESET_NAVIGATOR:
       return AppNavigator.router.getStateForAction(
-        CommonActions.reset({
+        StackActions.reset({
           actions: [
-            CommonActions.navigate({
+            NavigationActions.navigate({
               params: payload.data ? payload.data : {},
               routeName: payload.route,
             }),
@@ -39,9 +39,9 @@ export default function nav(state = initialState, action) {
 
       case LOGIN_SUCCESS:
       return AppNavigator.router.getStateForAction(
-        CommonActions.reset({
+        StackActions.reset({
           actions: [
-            CommonActions.navigate({ routeName:'Dashboard' }),
+            NavigationActions.navigate({ routeName:'Dashboard' }),
           ],
           index: 0,
           key: null,
@@ -51,14 +51,14 @@ export default function nav(state = initialState, action) {
     
       case GO_BACK:
       return AppNavigator.router.getStateForAction(
-        CommonActions.back(),
+        NavigationActions.back(),
         state
       );
 
     case LOGOUT_SUCCESS:
       return AppNavigator.router.getStateForAction(
-        CommonActions.reset({
-          actions: [CommonActions.navigate({ routeName: 'Welcome' })],
+        StackActions.reset({
+          actions: [NavigationActions.navigate({ routeName: 'Welcome' })],
           index: 0,
         }),
         state
@@ -66,9 +66,9 @@ export default function nav(state = initialState, action) {
 
     case REHYDRATE:
       return AppNavigator.router.getStateForAction(
-        CommonActions.reset({
+        StackActions.reset({
           actions: [
-            CommonActions.navigate({
+            NavigationActions.navigate({
               params: firstStateData,
               routeName: firstState,
             }),
